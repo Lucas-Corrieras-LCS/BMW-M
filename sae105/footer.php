be77b4d0f4d31d72d00188dcdf38b9e27184eb3e
Lucas Corrieras <159167109+Lucas-Corrieras-LCS@users.noreply.github.com>
2025-02-10 10:40:34 +0100

    Delete sae105 directory

--- sae105/footer.php
+++
@@ -1,6 +0,0 @@
-<footer onclick="scrollToTop()">
-    <div id="bot">
-        <p>&copy; 2025 MMI Troyes - Créé par Lucas Corrieras</p>
-        <a href="credits.php" id="credit">-Crédits-</a>
-    </div>
-</footer>
